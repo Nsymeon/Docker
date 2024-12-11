8833f827ac938cb16fd34fe3a0379af70cd8800a
N.Symeon <122832219+Hyperion-AG07@users.noreply.github.com>
2024-12-11 14:54:05 +0200

    Add files via upload

---
+++ backend/models/todoList.js
@@ -0,0 +1,30 @@
+//todoList.js 
+
+const mongoose = require('mongoose'); 
+
+const todoSchema = new mongoose.Schema({ 
+	task: { 
+		type: String, 
+		required: true, 
+	}, 
+	status: { 
+		type: String, 
+		required: true, 
+	}, 
+	deadline: { 
+		type: Date, 
+	}, 
+	description: { 
+		type: String, 
+		required: true,
+	}, 
+	category: { 
+		type: String, 
+		required: true,
+	}, 
+}); 
+
+
+const todoList = mongoose.model("todo", todoSchema); 
+
+module.exports = todoList;
